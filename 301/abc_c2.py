e4907c3aecea70cd5f1c5b6a7c3a13dcff2e9e2a
Dexterte <rejimully@gmail.com>
2024-11-23 16:49:40 +0900

    AC

---
+++ 301/abc_c2.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+
+s = input()
+t = input()
+
+
+def map_word_lengths(word):
+    """
+    文字の数を数えたリストを作成する
+    Args:
+        word ():
+
+    Returns:
+
+    """
+    # 文字の数を持っておく @は26番目
+    result = [0 for _ in range(27)]
+
+    for word_i in word:
+        if word_i == "@":
+            result[26] += 1
+        else:
+            result[ord(word_i) - ord("a")] += 1
+    return result
+
+
+cs = map_word_lengths(s)
+ct = map_word_lengths(t)
+
+for i in range(27):
+    target = chr(i + ord("a"))
+    if target in "atcoder":
+        if cs[i] < ct[i]:
+            cs[26] -= ct[i] - cs[i]
+        else:
+            ct[26] -= cs[i] - ct[i]
+    else:
+        # atcoderではなく、違いの文字の数がことなればNG
+        if cs[i] != ct[i]:
+            print("No")
+            exit()
+
+if cs[26] < 0 or ct[26] < 0:
+    print("No")
+    exit()
+
+print("Yes")
